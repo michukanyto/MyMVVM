35b33afc14f0bf674794c7056937af9be7da8700
Marlon <marlon.escobar.agudelo@desjardins.com>
2019-12-06 09:58:21 -0500

    Lifecycle testing

--- app/src/main/java/com/example/my_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/my_mvvm/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.my_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("MainActivity  ", "onCreate()")
+        viewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity  ", "onDestroy()")
     }
 }
